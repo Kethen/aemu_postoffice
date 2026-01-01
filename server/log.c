ac0f864010674d4bd92f76468ad95b4e5a2786cb
Katharine Chui <katharine.chui@gmail.com>
2026-01-01 14:54:52 +0100

    make it possible to build the whole reference server as cpp in one go

--- server/log.c
+++ server/log.c
@@ -5,7 +5,15 @@
 FILE *log_file = NULL;
 pthread_mutex_t log_mutex;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void init_logging()
 {
 	pthread_mutex_init(&log_mutex, NULL);
 }
+
+#ifdef __cplusplus
+}
+#endif
