67c1a1ec76698bf98338a1e434d64a9f5061c0c0
Katharine Chui <katharine.chui@gmail.com>
2026-01-10 00:48:39 +0100

    split delay implementation from mutex implementation

--- client/build_linux.sh
+++ client/build_linux.sh
@@ -3,6 +3,7 @@ gcc -fPIC -g -c test.c -o test.o -O0
 gcc -fPIC -g -c postoffice.c -o postoffice.o -O2
 gcc -fPIC -g -c sock_impl_linux.c -o sock_impl_linux.o -O2
 gcc -fPIC -g -c mutex_impl_linux.c -o mutex_impl_linux.o -O2
+gcc -fPIC -g -c delay_impl_linux.c -o delay_impl_linux.o -O2
 
-gcc -O0 test.o postoffice.o sock_impl_linux.o mutex_impl_linux.o -o test.out
-gcc -fPIC -shared postoffice.o sock_impl_linux.o mutex_impl_linux.o -o libaemu_postoffice_client.so
+gcc -O0 test.o postoffice.o sock_impl_linux.o mutex_impl_linux.o delay_impl_linux.o -o test.out
+gcc -fPIC -shared postoffice.o sock_impl_linux.o mutex_impl_linux.o delay_impl_linux.o -o libaemu_postoffice_client.so
--- client/build_windows.sh
+++ client/build_windows.sh
@@ -5,6 +5,7 @@ $GCC -fPIC -g -c test.c -o test.o -O0
 $GCC -fPIC -g -c postoffice.c -o postoffice.o -O2
 $GCC -fPIC -g -c sock_impl_windows.c -o sock_impl_windows.o -O2
 $GCC -fPIC -g -c mutex_impl_windows.c -o mutex_impl_windows.o -O2
+$GCC -fPIC -g -c delay_impl_windows.c -o delay_impl_windows.o -O2
 
-$GCC -O0 -static test.o postoffice.o sock_impl_windows.o mutex_impl_windows.o -lws2_32 -lpthread -o test.exe
-$GCC -fPIC -shared -static postoffice.o sock_impl_windows.o mutex_impl_windows.o -lws2_32 -o libaemu_postoffice_client.dll
+$GCC -O0 -static test.o postoffice.o sock_impl_windows.o mutex_impl_windows.o delay_impl_windows.o -lws2_32 -lpthread -o test.exe
+$GCC -fPIC -shared -static postoffice.o sock_impl_windows.o mutex_impl_windows.o delay_impl_windows.o -lws2_32 -o libaemu_postoffice_client.dll
