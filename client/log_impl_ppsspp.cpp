289429700199c73fa9f7eb47ef8eb97ccd7a8741
Katharine Chui <katharine.chui@gmail.com>
2026-01-19 21:15:32 +0100

    add ppsspp logging implemtnation

---
+++ client/log_impl_ppsspp.cpp
@@ -0,0 +1,19 @@
+#include "Common/Log.h"
+
+#include <stdio.h>
+#include <stdarg.h>
+
+static int log_impl(const char *format, ...){
+	va_list args;
+	char log_buf[2048] = {0};
+
+	va_start(args, format);
+	vsnprintf(log_buf, sizeof(log_buf), format, args);
+	va_end(args);
+
+	ERROR_LOG(Log::sceNet, "%s", log_buf);
+
+	return 0;
+}
+
+int (*log_func)(const char *format, ...) = log_impl;
