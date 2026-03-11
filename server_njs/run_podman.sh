8d51d700d798ac909140a0de5d2859d0d91ad0d0
Katharine Chui <katharine.chui@gmail.com>
2026-03-11 20:09:15 +0100

    use the official nodejs slim docker image on the run script

--- server_njs/run_podman.sh
+++ server_njs/run_podman.sh
@@ -1,15 +1,9 @@
-IMAGE=aemu_postoffice_njs_server
-
-if ! podman image exists $IMAGE
-then
-	podman image build -t $IMAGE -f Dockerfile
-fi
+IMAGE=node:slim
 
 podman run \
 	--rm -it \
 	-p 27313:27313 \
 	-p 27314:27314 \
-	--entrypoint "/usr/bin/node" \
 	-v ./aemu_postoffice.js:/aemu_postoffice.js:ro \
 	-v ./config.json:/config.json:ro \
 	$IMAGE \
