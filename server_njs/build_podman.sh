a2e19aac51cb13621b1ba631ea08fb1105c70b39
Katharine Chui <katharine.chui@gmail.com>
2026-03-27 12:56:53 +0100

    provide package.json and tsconfig.json for development

--- server_njs/build_podman.sh
+++ server_njs/build_podman.sh
@@ -1,17 +1,14 @@
 set -xe
 
-IMAGE=aemu_postoffice_ts_builder
-
-DEBUG=${DEBUG:-false}
-
-if ! podman image exists $IMAGE
-then
-	podman image build -t $IMAGE -f Dockerfile
-fi
+IMAGE=node:slim
 
 podman run \
 	--rm -it \
 	-v ./:/work_dir \
 	-w /work_dir \
+	--entrypoint '/bin/bash' \
 	$IMAGE \
-	tsc --typeRoots '/usr/local/lib/node_modules/@types' --types 'node' -m 'node18' -t 'es2021' --noImplicitAny aemu_postoffice.ts
+	-c '
+		npm install
+		npm run build
+	'
