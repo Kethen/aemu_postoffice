51f02f856b6c233b562ca1b83bc9014cb4ae19c1
Katharine Chui <katharine.chui@gmail.com>
2026-09-03 11:04:14 +0200

    bring client and cpp server up to parity on windows

--- adhocctl/client/build_windows.sh
+++ adhocctl/client/build_windows.sh
@@ -6,9 +6,10 @@ CPPC=x86_64-w64-mingw32-g++
 BUILD_FLAGS="-fPIC -g -O2 -Wformat"
 
 $CC $BUILD_FLAGS -c adhocctl.c -o adhocctl.o
+$CC $BUILD_FLAGS -c adhocctl_mem.c -o adhocctl_mem.o
 $CC $BUILD_FLAGS -c ../../client/sock_impl_windows.c -o sock_impl_windows.o
 $CC $BUILD_FLAGS -c ../../client/log_impl_stdc.c -o log_impl_stdc.o
 $CC $BUILD_FLAGS -c test.c -o test.o
 
-$CPPC $BUILD_FLAGS -static adhocctl.o sock_impl_windows.o log_impl_stdc.o test.o -lws2_32 -o test.exe
-$CPPC $BUILD_FLAGS -shared -static adhocctl.o sock_impl_windows.o log_impl_stdc.o -lws2_32 -o libadhocctl.dll
+$CPPC $BUILD_FLAGS -static adhocctl.o adhocctl_mem.o sock_impl_windows.o log_impl_stdc.o test.o -lws2_32 -o test.exe
+$CPPC $BUILD_FLAGS -shared -static adhocctl.o adhocctl_mem.o sock_impl_windows.o log_impl_stdc.o -lws2_32 -o libadhocctl.dll
---
+++ server_cpp/build_windows.sh
@@ -0,0 +1,18 @@
+set -xe
+
+SRC="log native_socket_windows session server semaphore main ../adhocctl/server_cpp/log common ../adhocctl/server_cpp/client ../adhocctl/server_cpp/server ../adhocctl/server_cpp/snapshot file_util ../adhocctl/server_cpp/game_db config snapshot http_status_server"
+
+BUILD_FLAGS="-g -O2 -fPIC --std=c++20 -Wformat"
+LINK_FLAGS="-static"
+LIBS="-lws2_32"
+
+CPPC=x86_64-w64-mingw32-g++
+
+built=""
+for f in $SRC
+do
+	$CPPC $BUILD_FLAGS -c ${f}.cpp -o ${f}.o
+	built="$built ${f}.o"
+done
+
+$CPPC $LINK_FLAGS $built $LIBS -o aemu_postoffice.exe
