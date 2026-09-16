704787d29243ab78dd05b7c7b9f39ecc81a13557
Katharine Chui <katharine.chui@gmail.com>
2026-09-16 18:44:45 +0200

    wip

--- server_cpp/enet.h
+++ server_cpp/enet.h
@@ -8,6 +8,7 @@
 // we make it so each peer behaves more like sockets
 
 #include <mutex>
+#include <shared_mutex>
 #include <thread>
 #include <unordered_map>
 #include <unordered_set>
@@ -47,28 +48,38 @@ struct recv_data {
 	std::string data;
 };
 
+struct peer {
+	bool disconnected;
+	void *peer; // a ENetPeer pointer
+	std::mutex send_buf_mutex; // send locks for adding send operations, enet_host_service loop locks for draining send operations
+	std::list<send_op> send_buf;
+	std::mutex recv_buf_mutex; // recv locks for draining packet, enet_host_service loop locks for adding data
+	std::unordered_map<int, std::list<recv_data>> recv_buf;
+};
+
 class BasicEnetServer{
 	public:
 		BasicEnetServer(std::string host, int port, int channels);
 		int accept(std::string &peer_addr, int &peer_port, EnetAcceptStatus &status); // returns a peer ref, or -1 on error
 		int recv(int peer_ref, char *buf, int buf_len, int channel, EnetRecvStatus &status); // returns buffer used, -1 on error
+		int get_outgoing_data(int peer_ref); // returns data queued on enet for sending, returns -1 on a disconnected peer
 		int send(int peer_ref, const char *buf, int buf_len, int channel, bool reliable, EnetSendStatus &status); // returns data queued for sending, or -1 on error
+		int get_incoming_data(int peer_ref); // returns data queued on enet for receiving, returns -1 on a disconnected peer
 		void close(int peer_ref);
+
 	private:
 		std::thread *worker;
 
 		void *server; // let's not cause enet.h to be loaded everywhere, a ENetHost pointer, only enet_host_service loop should be touching this
 
 		std::unordered_set<void *> new_peers; // peers to be used by accept(), a set of ENetPeer
-		std::unordered_map<int, void *> peers; // peer ref to ENetPeer pointer, is null when a peer is disconnected
+		std::unordered_map<int, struct peer> peers;
 		std::unordered_map<void *, int> peers_lookup; // reverse lookup, mostly used during disconnect
 
 		std::list<struct send_op> send_buffer;
 		std::unordered_map<int, std::unordered_map<int, std::list<recv_data>>> recv_buffer; // peer ref -> channel -> list of recv_data
 
-		std::mutex peer_mutex; // enet_host_server loop locks for new peers adding and disconnect handling, accept locks for upgrading new peers to peer, send/recv locks for quick checks
-		std::mutex send_buffer_mutex; // send calls locks for writing, enet_peer_send calls locks for copying
-		std::mutex recv_buffer_mutex; // recv calls locks for reading, enet_host_service loop locks for writing
+		std::shared_mutex peer_mutex; // enet_host_service loop locks for new peers adding and disconnecting handling, enet_host_service share locks for recv/send handling, accept locks for upgrading new peers to peers, send/recv share locks for peer operations, close locks for removing peer
 };
 
 }
